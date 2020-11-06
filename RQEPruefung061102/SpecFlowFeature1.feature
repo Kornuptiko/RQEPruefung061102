d4e38b868ef8c5c2f29a1a6b8e3734e24bdfc2b6
Göttl <benjamin.goettl@gmail.com>
2020-11-06 10:23:02 +0100

    Add project files.

---
+++ RQEPruefung061102/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Hexint
+	Given the number is "affe"
+	When you convert it from hex to int
+	Then the result should be 45054
+	
+	Scenario: octint
+	Given the number is 11147
+	When you convert it from oct to int
+	Then the result should be 4711
+
+	Scenario: binint
+	Given the number is 1100
+	When you convert it from bin to int
+	Then the result should be 12
---
+++ RQEPruefung061102/SpecFlowFeature1Steps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQEPruefung0611
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        string num;
+        int ans;
+
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(string p0)
+        {
+            num = p0;
+        }
+
+        [When(@"you convert it from hex to int")]
+        public void WhenYouConvertItFromHexToInt()
+        {
+            ans = System.Convert.ToInt32(num, 16);
+        }
+
+        [When(@"you convert it from oct to int")]
+        public void WhenYouConvertItFromOctToInt()
+        {
+            ans = System.Convert.ToInt32(num, 8);
+        }
+
+        [When(@"you convert it from bin to int")]
+        public void WhenYouConvertItFromBinToInt()
+        {
+            ans = System.Convert.ToInt32(num, 2);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = ans;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
